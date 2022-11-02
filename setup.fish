242799c9346ae120d60213a7a2baee32df9d76fb
Mike Carlin <mikefcarlin@protonmail.com>
2022-11-02 11:57:29 -0700

    Ensure nvim directories are created

--- setup.fish
+++ setup.fish
@@ -3,5 +3,6 @@
 ln -fs (realpath skhdrc) ~/.skhdrc
 ln -fs (realpath tmux.conf) ~/.tmux.conf
 ln -fs (realpath yabairc) ~/.yabairc
+mkdir -p ~/.config/nvim/lua
 ln -fs (realpath nvim/init.vim) ~/.config/nvim/init.vim
 ln -fs (realpath nvim/lua/base.lua) ~/.config/nvim/lua/base.lua
