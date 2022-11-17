09054c8b8c9914e47e73e2fb9a1606c95acaa311
Mike Carlin <michael.carlin@teledyne.com>
2022-11-17 10:07:43 -0800

    Add kitty configuration

--- setup.fish
+++ setup.fish
@@ -3,6 +3,11 @@
 ln -fs (realpath skhdrc) ~/.skhdrc
 ln -fs (realpath tmux.conf) ~/.tmux.conf
 ln -fs (realpath yabairc) ~/.yabairc
+
 mkdir -p ~/.config/nvim/lua
 ln -fs (realpath nvim/init.vim) ~/.config/nvim/init.vim
 ln -fs (realpath nvim/lua/base.lua) ~/.config/nvim/lua/base.lua
+
+mkdir -p ~/.config/kitty
+ln -fs (realpath kitty.conf) ~/.config/kitty/kitty.conf
+ln -fs (realpath kitty-theme.conf) ~/.config/kitty/kitty-theme.conf
