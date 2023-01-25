f30f4b7ba8d90253207b779a1203e49870bb6382
Mike Carlin <mike.carlin@hpe.com>
2023-01-24 21:20:31 -0800

    Fix incorrect nvim config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -193,7 +193,7 @@ return require('packer').startup(function(use)
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-      local servers = { 'clangd', 'pyright', 'tsserver' }
+      local servers = { 'clangd', 'pyright', 'tsserver', 'rust_analyzer' }
       for _, lsp in ipairs(servers) do
         lspconfig[lsp].setup {
           -- on_attach = my_custom_on_attach,
@@ -244,6 +244,11 @@ return require('packer').startup(function(use)
         on_attach = on_attach,
         flags = lsp_flags,
       }
+      require('lspconfig')['rust_analyzer'].setup{
+        on_attach = on_attach,
+        flags = lsp_flags,
+      }
+
     end
   }
 
@@ -273,6 +278,23 @@ return require('packer').startup(function(use)
             vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
             -- Code action groups
             vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+            local bufopts = { noremap=true, silent=true, buffer=bufnr }
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+            vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, bufopts)
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+            vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+            vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+            vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+            vim.keymap.set('n', '<space>wl', function()
+              print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            end, bufopts)
+            vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, bufopts)
+            vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, bufopts)
+            vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+ 
           end,
         },
         dap = {
