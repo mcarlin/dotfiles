2bfe86fdeaa2f6539eaa7ff9b6d01a8d7069b8ce
Mike Carlin <mike.carlin@hpe.com>
2023-02-03 06:45:32 -0800

    nvim: add toml lsp

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -252,7 +252,7 @@ return require('packer').startup(function(use)
         debounce_text_changes = 150,
       }
       -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-      local servers = { 'clangd', 'pyright', 'tsserver', 'sumneko_lua', }
+      local servers = { 'clangd', 'pyright', 'tsserver', 'sumneko_lua', 'taplo', }
       for _, lsp in ipairs(servers) do
         lspconfig[lsp].setup {
           -- on_attach = my_custom_on_attach,
