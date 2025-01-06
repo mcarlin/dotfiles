90d69114a3626ad42181c752314464e498156f9c
Mike Carlin <mike@mcarlin.com>
2025-01-06 04:23:24 -0800

    feat(wezterm): switch to fantasque font by default

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,23 +1,22 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
 -- Font
-config.font = wezterm.font 'Iosevka Nerd Font'
+config.font = wezterm.font("Fantasque Sans Mono")
 config.line_height = 1.3
-config.font_size = 18
+config.font_size = 20
 
 -- Appearance
 config.enable_tab_bar = false
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 -- Color Scheme
-config.color_scheme = 'Gruvbox dark, medium (base16)'
+config.color_scheme = "Gruvbox dark, medium (base16)"
 
 return config
-
